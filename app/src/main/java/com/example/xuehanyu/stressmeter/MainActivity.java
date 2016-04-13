16a8b047e84b35da3ce351f8ecc5923c83c04a20
HanyuX <zhucexue@126.com>
2016-04-13 15:34:38 -0400

    add chart

--- app/src/main/java/com/example/xuehanyu/stressmeter/MainActivity.java
+++ app/src/main/java/com/example/xuehanyu/stressmeter/MainActivity.java
@@ -1,10 +1,6 @@
 package com.example.xuehanyu.stressmeter;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,9 +9,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity{
+    private Fragment fragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,14 +78,42 @@ public class MainActivity extends AppCompatActivity{
     }
 
     public void selectDrawerItem(MenuItem menuItem) {
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            default:
+            fragmentClass = chartFragment.class;
+            break;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Log.d("dd", fragment.toString());
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        Log.d("dd", "catch");
+
+        onBackPressed();
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
-    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        Class fragmentClass;
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
---
+++ app/src/main/java/com/example/xuehanyu/stressmeter/chartFragment.java
@@ -0,0 +1,58 @@
+package com.example.xuehanyu.stressmeter;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.animation.ChartAnimationListener;
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.listener.LineChartOnValueSelectListener;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.ValueShape;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.Chart;
+import lecho.lib.hellocharts.view.LineChartView;
+import android.graphics.Color;
+
+/**
+ * Created by xuehanyu on 4/13/16.
+ */
+public class chartFragment extends Fragment {
+    @Override
+
+    /** called when the fragment is created */
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        View rootView = inflater.inflate(R.layout.chart_layout, container, false);
+
+        List<PointValue> values = new ArrayList<>();
+        values.add(new PointValue(0, 2));
+        values.add(new PointValue(1, 4));
+        values.add(new PointValue(2, 3));
+        values.add(new PointValue(3, 4));
+
+        //In most cased you can call data model methods in builder-pattern-like manner.
+        Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
+        List<Line> lines = new ArrayList<>();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        LineChartView chart = (LineChartView) rootView.findViewById(R.id.chart);
+        chart.setLineChartData(data);
+
+        return rootView;
+    }
+}
+
