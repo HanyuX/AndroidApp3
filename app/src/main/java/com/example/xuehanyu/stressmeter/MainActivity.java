29739c07406113bce760aa4ae16fc99b56b22df7
HanyuX <zhucexue@126.com>
2016-04-14 13:50:59 -0400

    ui han

--- app/src/main/java/com/example/xuehanyu/stressmeter/MainActivity.java
+++ app/src/main/java/com/example/xuehanyu/stressmeter/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+ //       getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -110,27 +110,5 @@ public class MainActivity extends AppCompatActivity{
         Log.d("dd", "catch");
 
         onBackPressed();
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        Class fragmentClass;
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-        return true;
     }
 }
